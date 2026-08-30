2e0529bd9d7b1c38564bf32b8e8c291d1f153e2d
arpit-goblins <arpitr@stanford.edu>
2026-08-29 21:25:26 -0700

    🎨 design: simplify carnival map arrangement

--- src/components/CarnivalMap.tsx
+++ src/components/CarnivalMap.tsx
@@ -6,7 +6,6 @@ type CarnivalPlacement = {
   y: number;
   width: number;
   angle?: number;
-  grounded?: boolean;
 };
 
 const ART_PATH = '/art/map/carnival';
@@ -16,48 +15,48 @@ const placements: CarnivalPlacement[] = [
     id: 'pani-puri',
     label: 'Pani puri',
     src: `${ART_PATH}/pani-puri.webp`,
-    x: 31.5,
-    y: 33,
+    x: 32.5,
+    y: 34,
     width: 4.2,
   },
   {
     id: 'cmu-fence',
     label: 'Carnegie Mellon fence',
     src: `${ART_PATH}/cmu-fence.webp`,
-    x: 36.5,
-    y: 35,
+    x: 37.5,
+    y: 34,
     width: 6.3,
   },
   {
     id: 'picnic-west',
     label: 'Picnic blanket with carrom and Jenga',
     src: `${ART_PATH}/picnic-carrom-jenga.webp`,
-    x: 41.7,
-    y: 26.5,
+    x: 43,
+    y: 27,
     width: 7.2,
   },
   {
     id: 'mehendi',
     label: 'Mehendi artists',
     src: `${ART_PATH}/henna.webp`,
-    x: 48.5,
-    y: 25.5,
+    x: 49.5,
+    y: 27,
     width: 4.2,
   },
   {
     id: 'picnic-east',
     label: 'Picnic blanket with carrom and Jenga',
     src: `${ART_PATH}/picnic-carrom-jenga.webp`,
-    x: 56.8,
-    y: 26.5,
+    x: 56,
+    y: 27,
     width: 7.2,
   },
   {
     id: 'jigsaw',
     label: 'Jigsaw puzzle',
     src: `${ART_PATH}/jigsaw.webp`,
-    x: 61.5,
-    y: 33,
+    x: 62,
+    y: 34,
     width: 3.6,
   },
   {
@@ -65,30 +64,30 @@ const placements: CarnivalPlacement[] = [
     label: 'Bazaar',
     src: `${ART_PATH}/bazaar.webp`,
     x: 68.5,
-    y: 34.5,
+    y: 35,
     width: 7.3,
   },
   {
     id: 'umbrella-arch',
     label: 'Rajasthani umbrella arch',
     src: `${ART_PATH}/umbrella-arch.webp`,
-    x: 68.8,
-    y: 47,
+    x: 68.5,
+    y: 48,
     width: 6.5,
   },
   {
     id: 'sunglasses',
     label: 'Colorful sunglasses',
     src: `${ART_PATH}/sunglasses.webp`,
-    x: 69.5,
-    y: 57.5,
+    x: 68.5,
+    y: 58,
     width: 4.1,
   },
   {
     id: 'block-print',
     label: 'Block-print tote bags',
     src: `${ART_PATH}/block-print-tote.webp`,
-    x: 69.2,
+    x: 68.5,
     y: 68,
     width: 4.8,
   },
@@ -96,124 +95,50 @@ const placements: CarnivalPlacement[] = [
     id: 'candy',
     label: 'Colorful candies',
     src: `${ART_PATH}/candy-bag.webp`,
-    x: 42,
-    y: 73.5,
+    x: 43,
+    y: 72,
     width: 3.5,
   },
   {
     id: 'paan',
     label: 'Paan cart',
     src: `${ART_PATH}/paan-cart.webp`,
-    x: 45.2,
-    y: 73,
+    x: 47,
+    y: 72,
     width: 3.8,
-  },
-  {
-    id: 'dabeli',
-    label: 'Dabeli',
-    src: `${ART_PATH}/dabeli.webp`,
-    x: 49.2,
-    y: 73.5,
-    width: 3.4,
   },
   {
     id: 'samosa',
     label: 'Samosa',
     src: `${ART_PATH}/samosa.webp`,
-    x: 52.5,
-    y: 73.5,
-    width: 3.4,
-  },
-  {
-    id: 'chaat-papdi',
-    label: 'Chaat papdi',
-    src: `${ART_PATH}/chaat-papdi.webp`,
-    x: 55.8,
-    y: 73.5,
-    width: 3.5,
+    x: 52,
+    y: 72,
+    width: 3.8,
   },
   {
     id: 'bicycle',
     label: 'Decorated bicycle',
     src: `${ART_PATH}/bicycle.webp`,
-    x: 59.2,
-    y: 73.5,
+    x: 57,
+    y: 72,
     width: 4.4,
   },
   {
     id: 'yarn',
     label: 'Yarn art',
     src: `${ART_PATH}/yarn-art.webp`,
-    x: 62.5,
-    y: 73,
+    x: 62,
+    y: 72,
     width: 3.2,
   },
   {
     id: 'airstream',
     label: 'Airstream drinks',
     src: `${ART_PATH}/airstream-drinks.webp`,
-    x: 34,
-    y: 80.5,
-    width: 10.5,
-    grounded: true,
+    x: 34.5,
+    y: 72.5,
+    width: 9,
   },
-];
-
-const cocktail_tables: CarnivalPlacement[] = [
-  { x: 44, y: 43 },
-  { x: 48, y: 43 },
-  { x: 52, y: 43 },
-  { x: 56, y: 43 },
-  { x: 44, y: 62 },
-  { x: 48, y: 62 },
-  { x: 52, y: 62 },
-  { x: 56, y: 62 },
-].map(({ x, y }, index) => ({
-  id: `cocktail-${index}`,
-  label: 'Standing cocktail table',
-  src: `${ART_PATH}/cocktail-table.webp`,
-  x,
-  y,
-  width: 2.1,
-}));
-
-const banquet_tables: CarnivalPlacement[] = [
-  { x: 27.2, y: 43 },
-  { x: 29.8, y: 49 },
-  { x: 27.4, y: 56 },
-  { x: 29.7, y: 62 },
-  { x: 27.7, y: 69 },
-].map(({ x, y }, index) => ({
-  id: `banquet-${index}`,
-  label: '10-person table',
-  src: `${ART_PATH}/banquet-table.webp`,
-  x,
-  y,
-  width: 3.4,
-}));
-
-const chair_rows: CarnivalPlacement[] = [
-  { x: 35.4, y: 56, angle: -34 },
-  { x: 36.1, y: 59, angle: -28 },
-  { x: 36.8, y: 62, angle: -20 },
-  { x: 37.5, y: 65, angle: -12 },
-  { x: 38.2, y: 68, angle: -4 },
-  { x: 38.9, y: 71, angle: 4 },
-].map(({ x, y, angle }, index) => ({
-  id: `chair-row-${index}`,
-  label: 'Rows of chairs',
-  src: `${ART_PATH}/chair.webp`,
-  x,
-  y,
-  angle,
-  width: 1.5,
-}));
-
-const all_placements = [
-  ...placements,
-  ...cocktail_tables,
-  ...banquet_tables,
-  ...chair_rows,
 ];
 
 export function CarnivalMap() {
@@ -231,10 +156,10 @@ export function CarnivalMap() {
           width={1689}
           height={931}
         />
-        {all_placements.map((placement) => (
+        {placements.map((placement) => (
           <img
             key={placement.id}
-            className={`carnival-map-item${placement.grounded ? ' is-grounded' : ''}`}
+            className="carnival-map-item"
             src={placement.src}
             alt=""
             aria-hidden="true"
@@ -250,9 +175,6 @@ export function CarnivalMap() {
           {placements.map((placement) => (
             <li key={placement.id}>{placement.label}</li>
           ))}
-          <li>Standing cocktail tables</li>
-          <li>10-person tables</li>
-          <li>Rows of chairs</li>
         </ul>
       </div>
     </section>
